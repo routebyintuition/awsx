6139cb20ec585a488e6164db77b860930f2f7850
Fontes <fontesj@186590cd9477.ant.amazon.com>
2019-05-02 14:42:41 -0700

    Updated lib to chain session

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # awsx
-Go AWS utilities
+
+---
+
+Package provides Go AWS utilities that I typically have to repeat and use often.
+
+## Install
+
+    go get -u github.com/routebyintuition/awsx
+
+## Usage
+
+For standard AWS connectivity, we have the capability to create a new session based upon the various authentication
+mechanisms built into the AWS Go SDK.
+
+In the below example, we are using the Amazon EC2 credentials to create a new session:
+
+    input := &s3.ListBucketsInput{}
+    a := awsx.NewAWS().EnablePanic().WithInstanceRole()
+    a.SetRegion("us-east-1")
+    a.SetSession()
+    s3 := s3.New(a.Session)
+    result, err := s3.ListBuckets(input)
+    if err != nil {
+        fmt.Println("Error: ", err)
+        os.Exit(1)
+    }
+    fmt.Println(result)
+
+## Additional Information
+
+Original connection methods used from https://github.com/C2FO/vfs with the AWS connection implementation for the S3 io.Writer.
+
+## To Do
+
+* Add cloudwatch IO writer
+* Add RDS master/replica lookups - MySQL
+* Add RDS MySQL IAM auth
+* Add RDS master/replica lookups - PostgreSQL
+* Add PostgreSQL IAM auth
+* Add Redis cluster init
+* Add Memcached server lookup
--- aws.go
+++ aws.go
@@ -14,39 +14,42 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-// AWSConfig is the configuration definition for our AWS services.
-type AWSConfig struct {
-	awsRegion       string // should set AWS region used or a default is used
-	awsRole         string // optional: only if using to assume an AWS role
-	awsAccessKey    string // optional: only used if requiring AWS access key/secret key authentication
-	awsSecretKey    string // optional: only used if requiring AWS access key/secret key authentication
-	awsSessionToken string // optional: only used if requiring AWS access key/secret key authentication
-	awsEndpoint     string // optional: use a specified endpoint for calls
-	awsCredFile     string // optional: credentials file to use
-	awsProfile      string // optional: which credential profile to utilize
-	awsProviders    []credentials.Provider
-	panicOnErr      bool // Should we panic the app or proceed if we can't publish to CWL
+// Config is the configuration definition for our AWS services.
+type Config struct {
+	Region       string // should set AWS region used or a default is used
+	Role         string // optional: only if using to assume an AWS role
+	AccessKey    string // optional: only used if requiring AWS access key/secret key authentication
+	SecretKey    string // optional: only used if requiring AWS access key/secret key authentication
+	SessionToken string // optional: only used if requiring AWS access key/secret key authentication
+	Endpoint     string // optional: use a specified endpoint for calls
+	CredFile     string // optional: credentials file to use
+	Profile      string // optional: which credential profile to utilize
+	Providers    []credentials.Provider
+	Session      *session.Session
+	panicOnErr   bool // Should we panic the app or proceed if we can't publish to CWL
 }
 
-func NewAWS() *AWSConfig {
+// NewAWS creates a new Config struct and populates it with an empty provider chain
+func NewAWS() *Config {
 	p := make([]credentials.Provider, 0)
-	return &AWSConfig{awsProviders: p}
+	return &Config{Providers: p}
 }
 
-func (a *AWSConfig) WithStatic() *AWSConfig {
+// WithStatic adds a static credential provider to the provider chain
+func (a *Config) WithStatic() *Config {
 	// If the static credentials are provided and who knows why but maybe
 	// a token is included in there?
-	if a.awsAccessKey != "" && a.awsSecretKey != "" {
+	if a.AccessKey != "" && a.SecretKey != "" {
 		v := credentials.Value{
-			AccessKeyID:     a.awsAccessKey,
-			SecretAccessKey: a.awsSecretKey,
+			AccessKeyID:     a.AccessKey,
+			SecretAccessKey: a.SecretKey,
 		}
-		if a.awsSessionToken != "" {
-			v.SessionToken = a.awsSessionToken
+		if a.SessionToken != "" {
+			v.SessionToken = a.SessionToken
 
 		}
 
-		a.awsProviders = append(a.awsProviders, &credentials.StaticProvider{Value: v})
+		a.Providers = append(a.Providers, &credentials.StaticProvider{Value: v})
 
 	} else {
 		fmt.Println("No static AWS credebtials found")
@@ -57,22 +60,52 @@ func (a *AWSConfig) WithStatic() *AWSConfig {
 
 // EnablePanic will set panicOnErr to true so that if we hit any errors, the application
 // will panic out
-func (a *AWSConfig) EnablePanic() *AWSConfig {
+func (a *Config) EnablePanic() *Config {
 	a.panicOnErr = true
 	return a
 }
 
 // DisablePanic will set panicOnErr to false so that we do not panic the application
 // in care there is an error
-func (a *AWSConfig) DisablePanic() *AWSConfig {
+func (a *Config) DisablePanic() *Config {
 	a.panicOnErr = false
+	return a
+}
+
+// SetRegion sets the AWS region to use with the service calls
+func (a *Config) SetRegion(region string) *Config {
+	if len(region) > 0 {
+		a.Region = region
+	} else {
+		fmt.Println("No region specified in call to SetRegion(region string)")
+	}
+	return a
+}
+
+// SetProfile sets the profile name to be used with authentication
+func (a *Config) SetProfile(profile string) *Config {
+	if len(profile) > 0 {
+		a.Profile = profile
+	} else {
+		fmt.Println("No profile specified in call to SetProfile(profile string)")
+	}
+	return a
+}
+
+// SetEndpoint sets the endpoint to use if this is a custom value
+func (a *Config) SetEndpoint(endpoint string) *Config {
+	if len(endpoint) > 0 {
+		a.Endpoint = endpoint
+	} else {
+		fmt.Println("No Endpoint specified in all to SetEndpoint(endpoint string)")
+	}
 	return a
 }
 
 // WithEnv adds the environment provider to the credential chain so that
 // if AWS environment credentials are available, they will be used for auth
-func (a *AWSConfig) WithEnv() *AWSConfig {
-	a.awsProviders = append(a.awsProviders, &credentials.EnvProvider{})
+func (a *Config) WithEnv() *Config {
+	a.Providers = append(a.Providers, &credentials.EnvProvider{})
 	return a
 }
 
@@ -81,112 +114,152 @@ func (a *AWSConfig) WithEnv() *AWSConfig {
 // In addition to the default locations, if a.awsCredFile is set, we will look at that
 // location for the AWS credentials.
 // If a.awsProfile is set, this will enable the use of a profile name other than default
-func (a *AWSConfig) WithFile() *AWSConfig {
+func (a *Config) WithFile() *Config {
 	// Path to the shared credentials file.
-	if a.awsCredFile != "" {
+	if a.CredFile != "" {
 		cfile := &credentials.SharedCredentialsProvider{
-			Filename: a.awsCredFile,
+			Filename: a.CredFile,
 		}
-		if a.awsProfile != "" {
-			cfile.Profile = a.awsProfile
+		if a.Profile != "" {
+			cfile.Profile = a.Profile
 		}
-		a.awsProviders = append(a.awsProviders, cfile)
+		a.Providers = append(a.Providers, cfile)
 	} else {
-		a.awsProviders = append(a.awsProviders, &credentials.SharedCredentialsProvider{})
+		a.Providers = append(a.Providers, &credentials.SharedCredentialsProvider{})
 	}
 
 	return a
 }
 
-//auth provides a chain of credentials for connectivity
-//func (c *CwConfig) auth() []credentials.Provider {
-func (a *AWSConfig) auth() []credentials.Provider {
-	p := make([]credentials.Provider, 0)
-
-	// If the static credentials are provided and who knows why but maybe
-	// a token is included in there?
-	if a.awsAccessKey != "" && a.awsSecretKey != "" {
-		v := credentials.Value{
-			AccessKeyID:     a.awsAccessKey,
-			SecretAccessKey: a.awsSecretKey,
-		}
-		if a.awsSessionToken != "" {
-			v.SessionToken = a.awsSessionToken
-
-		}
-
-		p = append(p, &credentials.StaticProvider{Value: v})
-
-	}
-
-	// Check the local ENV variables for the right credentials
-	p = append(p, &credentials.EnvProvider{})
-
-	// Path to the shared credentials file.
-	if a.awsCredFile != "" {
-		cfile := &credentials.SharedCredentialsProvider{
-			Filename: a.awsCredFile,
-		}
-		if a.awsProfile != "" {
-			cfile.Profile = a.awsProfile
-		}
-		p = append(p, cfile)
-	} else {
-		p = append(p, &credentials.SharedCredentialsProvider{})
-	}
-
-	lowTimeoutClient := &http.Client{Timeout: 1 * time.Second} // low timeout to ec2 metadata service
+// WithInstanceRole adds the credentials from the EC2 instance obtained from the
+// metadata service to the provider list.
+func (a *Config) WithInstanceRole() *Config {
+	lowTimeoutClient := &http.Client{Timeout: 3 * time.Second} // low timeout to ec2 metadata service
 
 	// RemoteCredProvider for default remote endpoints such as EC2 or ECS IAM Roles
 	def := defaults.Get()
 	def.Config.HTTPClient = lowTimeoutClient
-	p = append(p, defaults.RemoteCredProvider(*def.Config, def.Handlers))
+	a.Providers = append(a.Providers, defaults.RemoteCredProvider(*def.Config, def.Handlers))
 
 	// EC2RoleProvider retrieves credentials from the EC2 service, and keeps track if those credentials are expired
 	sess, err := session.NewSession()
 	if err != nil {
 		fmt.Println("Error on connecting to AWS: ", err)
 		if a.panicOnErr {
-			fmt.Println("CloudWatchLogs panicOnError is enabled so existing...")
+			fmt.Println("panicOnError is enabled so exiting...")
 			os.Exit(1)
 		}
 		return nil
 	}
-	p = append(p, &ec2rolecreds.EC2RoleProvider{
+	a.Providers = append(a.Providers, &ec2rolecreds.EC2RoleProvider{
 		Client: ec2metadata.New(sess, &aws.Config{
 			HTTPClient: lowTimeoutClient,
 		}),
 		ExpiryWindow: 3,
 	})
 
-	return p
+	return a
 }
 
-func (a *AWSConfig) Session() *session.Session {
-	awsConfig := defaults.Config()
+//WithAllProviders provides a chain of credentials for connectivity
+func (a *Config) WithAllProviders() *Config {
 
-	if a.awsRegion != "" {
-		awsConfig.WithRegion(a.awsRegion)
-	} else if val, ok := os.LookupEnv("AWS_DEFAULT_REGION"); ok {
-		awsConfig.WithRegion(val)
+	// If the static credentials are provided and who knows why but maybe
+	// a token is included in there?
+	if a.AccessKey != "" && a.SecretKey != "" {
+		v := credentials.Value{
+			AccessKeyID:     a.AccessKey,
+			SecretAccessKey: a.SecretKey,
+		}
+		if a.SessionToken != "" {
+			v.SessionToken = a.SessionToken
+
+		}
+
+		a.Providers = append(a.Providers, &credentials.StaticProvider{Value: v})
+	}
+
+	// Check the local ENV variables for the right credentials
+	a.Providers = append(a.Providers, &credentials.EnvProvider{})
+
+	// Path to the shared credentials file.
+	if a.CredFile != "" {
+		cfile := &credentials.SharedCredentialsProvider{
+			Filename: a.CredFile,
+		}
+		if a.Profile != "" {
+			cfile.Profile = a.Profile
+		}
+		a.Providers = append(a.Providers, cfile)
 	} else {
-		awsConfig.WithRegion("us-east-1")
+		a.Providers = append(a.Providers, &credentials.SharedCredentialsProvider{})
 	}
 
-	if a.awsEndpoint != "" {
-		awsConfig.WithEndpoint(a.awsEndpoint)
+	httpTimeout := &http.Client{Timeout: 3 * time.Second} // low timeout to ec2 metadata service
+
+	// RemoteCredProvider for default remote endpoints such as EC2 or ECS IAM Roles
+	def := defaults.Get()
+	def.Config.HTTPClient = httpTimeout
+	a.Providers = append(a.Providers, defaults.RemoteCredProvider(*def.Config, def.Handlers))
+
+	// EC2RoleProvider retrieves credentials from the EC2 service, and keeps track if those credentials are expired
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("Error on connecting to AWS: ", err)
+		if a.panicOnErr {
+			fmt.Println("panicOnError is enabled so exiting...")
+			os.Exit(1)
+		}
+		return nil
+	}
+	a.Providers = append(a.Providers, &ec2rolecreds.EC2RoleProvider{
+		Client: ec2metadata.New(sess, &aws.Config{
+			HTTPClient: httpTimeout,
+		}),
+		ExpiryWindow: 3,
+	})
+
+	return a
+}
+
+// SetSession calls GetSession and sets the session return as a struct param
+func (a *Config) SetSession() *Config {
+	a.Session = a.GetSession()
+	return a
+}
+
+// GetSession creates a new session based upon the Config struct we built using the
+// above functions
+func (a *Config) GetSession() *session.Session {
+	if len(a.Providers) == 0 {
+		fmt.Println("Calling GetSession() without initializing a credential provider using With*() methods.")
+		if a.panicOnErr {
+			panic("No credential providers specified")
+		}
 	}
 
-	a.auth()
+	Config := defaults.Config()
 
-	awsConfig.WithCredentials(
-		credentials.NewChainCredentials(a.awsProviders),
+	if a.Region != "" {
+		Config.WithRegion(a.Region)
+	} else if val, ok := os.LookupEnv("AWS_DEFAULT_REGION"); ok {
+		Config.WithRegion(val)
+	} else {
+		Config.WithRegion("us-east-1")
+	}
+
+	if a.Endpoint != "" {
+		Config.WithEndpoint(a.Endpoint)
+	}
+
+	Config.WithCredentials(
+		credentials.NewChainCredentials(a.Providers),
 	)
 
 	// create new session with config
 	sess, err := session.NewSessionWithOptions(
 		session.Options{
-			Config: *awsConfig,
+			Config: *Config,
 		},
 	)
 	if err != nil {
